bca5036ba23ad1bc53d2fc942bff571834ab8344
Eunbee Jo <eunbeejo94@gmail.com>
2016-05-14 22:23:47 +0900

    로그인view

--- LoginView.java
+++ LoginView.java
@@ -13,15 +13,16 @@ public class LoginView extends JFrame //implements ActionListener
         setBounds(0,0,250,500);
         this.setVisible(true);
         this.setLayout( null);
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
 
-        JLabel label1 = new JLabel("ID");
-        JLabel label2 = new JLabel("Password");
+        JLabel label1 = new JLabel("ID : ");
+        JLabel label2 = new JLabel("PW : ");
         JTextField text1 = new JTextField(10);
         JPasswordField text2 = new JPasswordField(10);
         JRadioButton checkAudience = new JRadioButton("Audience",false);
         JRadioButton checkHost = new JRadioButton("Host",false);
         ButtonGroup btngroup = new ButtonGroup();
-        JButton login = new JButton("Login");
+        JButton login = new JButton("·Î±×ÀÎ");
         btngroup.add(checkAudience);
         btngroup.add(checkHost);
 
@@ -29,9 +30,17 @@ public class LoginView extends JFrame //implements ActionListener
         label2.setBounds(50, 200, 50, 30);
         text1.setBounds(110, 170, 150, 30);
         text2.setBounds(110, 200, 150, 30);
-        checkAudience.setBounds(80, 250,60, 30);
-        checkHost.setBounds(180, 250, 60, 30);
-        login.setBounds(130, 280, 60, 30);
+        checkAudience.setBounds(50, 250,90, 30);
+        checkHost.setBounds(180, 250,90, 30);
+        login.setBounds(100, 280, 80, 30);
+        label1.setFont(new java.awt.Font("Gulim", 0, 14));
+        label2.setFont(new java.awt.Font("Gulim", 0, 14));
+        text1.setFont(new java.awt.Font("Gulim", 0, 14));
+        text2.setFont(new java.awt.Font("Gulim", 0, 14));
+        checkAudience.setFont(new java.awt.Font("Gulim", 0, 14));
+        checkHost.setFont(new java.awt.Font("Gulim", 0, 14));
+        login.setFont(new java.awt.Font("Gulim", 0, 14));
+
 
         loginpanel.add(label1);
         loginpanel. add(label2);
@@ -44,7 +53,7 @@ public class LoginView extends JFrame //implements ActionListener
         login.addActionListener(new ActionListener()
         {
             public void actionPerformed(ActionEvent e) {
-                new HomeView().setVisible(true); // Main Form to show after the Login Form.
+                new LoginView().setVisible(true); // Main Form to show after the Login Form.
                 dispose();
             }
         });
@@ -62,7 +71,7 @@ public class LoginView extends JFrame //implements ActionListener
 
     public void actionPerformed(ActionEvent e)
     {
-        new HomeView().setVisible(true); // Main Form to show after the Login Form.
+        new LoginView().setVisible(true); // Main Form to show after the Login Form.
         dispose();
     }
 
